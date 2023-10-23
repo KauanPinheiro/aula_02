60a991e2d631c30f07ecc7fe57513e045e4379d7
KAUAN PINHEIRO DO NASCIMENTO <kauan.pnascimento@senacsp.edu.br>
2023-10-23 11:47:40 -0300

    atualizando a pasta

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -12,16 +12,16 @@ import Raio from "../../img/bgRaichu.png"
 
 function Main() {
     const listA = [
-        { nome: "Charlizard", caracteristica: "Fogo", img: Charlizard, c: "thesemi" },
-        { nome: "Blastoise", caracteristica: "Água", img: Blastoise, c: "thebest", semi:'thesemi'},
-        { nome: "Venossauro", caracteristica: "Grama", img: Venossauro, c: "thebest" },
-        { nome: "Pikachu", caracteristica: "Eletríco", img: Pikachu, c: "thesemi" }
+        { nome: "Charlizard", caracteristica: "Fogo", img: Charlizard, c: "thesemi" , name:"tName" },
+        { nome: "Blastoise", caracteristica: "Água", img: Blastoise, c: "thebest", semi:'thesemi', name:"tName"},
+        { nome: "Venossauro", caracteristica: "Grama", img: Venossauro, c: "thebest" , name:"tName"},
+        { nome: "Pikachu", caracteristica: "Eletríco", img: Pikachu, c: "thesemi" , name:"tName" }
     ]
     const listB = [
-        { nome: "Typlosion", caracteristica: "Fogo", img: Tyle, c: "thebest" },
-        { nome: "Feraligatr", caracteristica: "Água", img: Ferra, c: "thesemi" },
-        { nome: "Meganium ", caracteristica: "Grama", img: Mega, c: "thesemi" },
-        { nome: "Raichu", caracteristica: "Eletríco", img: Raio, c: "thebest" }
+        { nome: "Typlosion", caracteristica: "Fogo", img: Tyle, c: "thebest" , name:"tName" },
+        { nome: "Feraligatr", caracteristica: "Água", img: Ferra, c: "thesemi", name:"tName" },
+        { nome: "Meganium ", caracteristica: "Grama", img: Mega, c: "thesemi", name:"tName" },
+        { nome: "Raichu", caracteristica: "Eletríco", img: Raio, c: "thebest", name:"tName" }
     ]
 
 
